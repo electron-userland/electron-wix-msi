9557ecba35666c03fa0dade94495d9669ba99278
Jan Hannemann <jan.hannemann@outlook.com>
2020-02-18 23:08:07 -0800

    more edge casehandling for rcedit and version util

--- src/utils/version-util.ts
+++ src/utils/version-util.ts
@@ -1,17 +1,36 @@
 import * as semver from 'semver';
+import { isNumber } from 'util';
+
+function isWindowsComplaint(version: string): boolean {
+  const versionArray = version.split('.');
+  if (versionArray.length !== 4) {
+   return false;
+  }
+
+  for (let i = 0; i < 4; i++) {
+    if (isNaN(Number(versionArray[i]))) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 /**
- * Takes a semantic version number (2.3.1.alpha234234234) and returns
+ * Takes a semantic version number (2.3.1-alpha234234234) and returns
  * something more Windows-compatible (2.3.1.0).
  *
  * @param {string} input
  * @returns {string}
  */
 export function getWindowsCompliantVersion(input: string): string {
-  const parsed = semver.parse(input);
+  if (isWindowsComplaint(input)) {
+    return input;
+  }
 
+  const parsed = semver.parse(input);
   if (parsed) {
-    return `${parsed.major}.${parsed.minor}.${parsed.patch}`;
+    return `${parsed.major}.${parsed.minor}.${parsed.patch}.0`;
   } else {
     throw new Error('Could not parse semantic version input string');
   }
