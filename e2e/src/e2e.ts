0704872dd3e6d3e6cdbd2642109ba72bf1d89a97
Jan Hannemann <jan.hannemann@outlook.com>
2020-02-26 15:43:46 -0800

    tests packaging,installing, updating, uninstalling

--- e2e/src/e2e.ts
+++ e2e/src/e2e.ts
@@ -1,27 +1,126 @@
-import * as expect from 'expect';
+import expect from 'expect.js';
 import * as fs from 'fs-extra';
-import path = require('path');
-import { install as install } from './utils/installer';
-import { OUT_DIR, packageSampleApp } from './utils/packager';
+import hasha from 'hasha';
+import path from 'path';
+
+import { getWindowsCompliantVersion } from '../../lib/utils/version-util';
+import { checkInstall, getInstallPaths, install, uninstall, uninstallViaPowershell } from './utils/installer';
+import { defaultOptions, HARNESS_APP_DIR, OUT_DIR, packageSampleApp } from './utils/packager';
 
 const msiPath = path.join(OUT_DIR, 'HelloWix.msi');
 
-describe('MSI packaging', () =>  {
-    it('creates a package', async () => {
-      await packageSampleApp();
-      expect(fs.pathExistsSync(msiPath)).toBeTruthy();
-    });
-});
+const expectSameFolderContent = async (folderA: string, folderB: string) => {
+  const folderContent =  await fs.readdir(folderA);
 
-describe('Installing', () =>  {
-  it('installs', async () => {
-   await install(msiPath);
+  folderContent.forEach(async (item) => {
+    const itemPathA = path.join(folderA, item);
+    const itemPathB = path.join(folderB, item);
+    expect(fs.pathExistsSync(itemPathB)).ok();
+    if (fs.lstatSync(itemPathA).isDirectory()) {
+      await expectSameFolderContent(itemPathA, itemPathB);
+    } else {
+      const hashA = await hasha.fromFile(itemPathA);
+      const hashB = await hasha.fromFile(itemPathB);
+      expect(hashA).to.be(hashB);
+    }
+  });
+};
+
+interface TestConfig {
+  arch: 'x86' | 'x64';
+  shortcutFolderName?: string;
+  shortcutName?: string;
+}
+
+describe('Electron WIX MSI', () => {
+  before(async () => {
+    if (await checkInstall(defaultOptions.name)) {
+      await uninstallViaPowershell(defaultOptions.name);
+    }
   });
 
-  it('has all files in program files', () => {
-    console.log(process.env['ProgramFiles(x86)']);
-    const appfolder = path.join(process.env['ProgramFiles(x86)']!, 'HelloWix');
-    const stubPath = path.join(appfolder, 'hellowix.exe');
-    expect(fs.pathExistsSync(stubPath)).toBeTruthy();
+  const tests: TestConfig[] = [
+    {arch: 'x86'},
+    {arch: 'x86', shortcutFolderName: 'SuperWix', shortcutName: 'SuperHello'},
+    {arch: 'x64'},
+  ];
+
+  tests.forEach((test) => {
+    const options = {
+      ...defaultOptions,
+      ...test
+    };
+
+    const getTestConfigString = () => {
+      let configString = test.arch || 'arch:undefined';
+      if (test.shortcutFolderName || test.shortcutName) {
+        configString += '|shortcut:';
+        configString += test.shortcutFolderName || '';
+        configString += test.shortcutFolderName ? '\\' : '';
+        configString += test.shortcutName || '';
+      }
+      return configString;
+    };
+
+    describe(`Packaging ${getTestConfigString()}`, () =>  {
+        it('creates a package', async () => {
+          await packageSampleApp(options);
+          expect(fs.pathExistsSync(msiPath)).to.be(true);
+        });
+    });
+
+    describe(`Installing ${getTestConfigString()}`, () =>  {
+      it('installs', async () => {
+        await install(msiPath);
+        const version = getWindowsCompliantVersion(options.version);
+        expect(await checkInstall(options.name, version)).ok();
+      });
+
+      it('has all files in program files', () => {
+        const paths = getInstallPaths(options);
+        console.log(paths);
+        expect(fs.pathExistsSync(paths.stubExe)).ok();
+        expectSameFolderContent(HARNESS_APP_DIR, paths.appFolder);
+      });
+
+      it('has shortcuts', () => {
+        const paths = getInstallPaths(options);
+        expect(fs.pathExistsSync(paths.startMenuShortcut)).ok();
+        expect(fs.pathExistsSync(paths.desktopShortcut)).ok();
+      });
+    });
+
+    describe(`Updating ${getTestConfigString()}`, () =>  {
+      const options124 = {
+        ...options,
+        version: '1.2.4'
+      };
+
+      it('updates', async () => {
+        await packageSampleApp(options124);
+        await install(msiPath);
+        const version = getWindowsCompliantVersion(options124.version);
+        expect(await checkInstall(options124.name, version)).ok();
+      });
+
+      it('has all files in program files', () => {
+        const paths = getInstallPaths(options124);
+        expect(fs.pathExistsSync(paths.stubExe)).ok();
+        expectSameFolderContent(HARNESS_APP_DIR, paths.appFolder);
+      });
+
+      it('has shortcuts', () => {
+        const paths = getInstallPaths(options124);
+        expect(fs.pathExistsSync(paths.startMenuShortcut)).ok();
+        expect(fs.pathExistsSync(paths.desktopShortcut)).ok();
+      });
+    });
+
+    describe(`Uninstalling ${getTestConfigString()}`, () =>  {
+      it('uninstalls', async () => {
+        await uninstall(msiPath);
+        expect(await checkInstall(options.name)).not.ok();
+      });
+    });
   });
 });
--- e2e/src/utils/installer.ts
+++ e2e/src/utils/installer.ts
@@ -1,5 +1,72 @@
+import Shell from 'node-powershell';
+import path from 'path';
 import { spawnPromise } from 'spawn-rx';
+
+import { MSICreatorOptions } from '../../../lib/index';
+
+export interface InstallPaths {
+  appRootFolder: string;
+  appFolder: string;
+  stubExe: string;
+  startMenuShortcut: string;
+  desktopShortcut: string;
+}
 
 export const install = async (msi: string) => {
   return spawnPromise('msiexec.exe', ['/i', msi, '/qb']);
 };
+
+export const uninstall = async (msi: string) => {
+  return spawnPromise('msiexec.exe', ['/x', msi, '/qb']);
+};
+
+export const uninstallViaPowershell = async (name: string) => {
+  const ps = new Shell({
+    executionPolicy: 'Bypass',
+    noProfile: true
+  });
+
+  try {
+    ps.addCommand(`$p = Get-Package -Name ${name} | Uninstall-Package`);
+    await ps.invoke();
+  } finally {
+    ps.dispose();
+  }
+};
+
+export const checkInstall = async (name: string, version?: string) => {
+  const ps = new Shell({
+    executionPolicy: 'Bypass',
+    noProfile: true
+  });
+
+  let installPackage: string;
+  try {
+    if (version) {
+      ps.addCommand(`$p = Get-Package -Name ${name} -RequiredVersion ${version} -ErrorAction SilentlyContinue`);
+      ps.addCommand('$p.Name + $p.Version');
+    } else {
+      ps.addCommand(`$p = Get-Package -Name ${name} -ErrorAction SilentlyContinue`);
+      ps.addCommand('$p.Name');
+    }
+    installPackage = await ps.invoke();
+    installPackage = installPackage.replace(/(\r\n|\n|\r)/gm, '');
+  } finally {
+    ps.dispose();
+  }
+
+  return installPackage === `${name}${!!version ? version : ''}`;
+};
+
+export const getInstallPaths = (options: MSICreatorOptions): InstallPaths => {
+  const programFiles = options.arch === 'x64' || options.arch === 'ia64' ? process.env.ProgramFiles! : process.env['ProgramFiles(x86)']!;
+  const appRootFolder = path.join(programFiles, options.name);
+  return {
+    appRootFolder,
+    stubExe: path.join(appRootFolder, options.exe),
+    appFolder: path.join(appRootFolder, `app-${options.version}`),
+    startMenuShortcut: path.join(process.env.ProgramData!, `Microsoft/Windows/Start Menu/Programs/${options.shortcutFolderName || options.manufacturer}/${options.shortcutName || options.name}.lnk`),
+    desktopShortcut: path.join(process.env.Public!, `Desktop/${options.shortcutName || options.name}.lnk`),
+  };
+};
+
