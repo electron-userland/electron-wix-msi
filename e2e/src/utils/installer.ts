12cf11d3d821b02ffea4a94be25466939baede72
Jan Hannemann <jan.hannemann@outlook.com>
2020-04-27 11:28:03 -0700

    e2e tests

--- e2e/src/utils/installer.ts
+++ e2e/src/utils/installer.ts
@@ -25,17 +25,22 @@ export interface InstallPaths {
   registryRunKey: string;
 }
 
-export const install = async (msi: string, installLevel: 1 | 2 | 3 = 1, autoUpdaterUserGroup?: string) => {
+export const install = async (msi: string, installLevel: 1 | 2 | 3 = 2, autoUpdaterUserGroup?: string, installMode?: 'perUser' | 'perMachine') => {
   const args = ['/i', msi];
 
-  if (installLevel !== 1) {
+  if (installLevel !== 2) {
     args.push(`INSTALLLEVEL=${installLevel}`);
   }
   if (autoUpdaterUserGroup) {
     args.push(`UPDATERUSERGROUP=${autoUpdaterUserGroup}`);
   }
+  if (installMode === 'perMachine') {
+    args.push(`MSIINSTALLPERUSER=0`);
+  }
+  if (installMode === 'perUser') {
+    args.push(`MSIINSTALLPERUSER=1`);
+  }
   args.push('/qb');
-
   return spawnPromise('msiexec.exe', args);
 };
 
@@ -85,24 +90,30 @@ export const checkInstall = async (name: string, version?: string) => {
   return installPackage === `${name}${!!version ? version : ''}`;
 };
 
-export const getInstallPaths = (options: MSICreatorOptions | SquirrelOptions): InstallPaths => {
+export const getInstallPaths = (options: MSICreatorOptions | SquirrelOptions,
+                                installMode: 'perUser' | 'perMachine' = 'perMachine'): InstallPaths => {
   const arch = options.arch;
-  const programFiles = arch === 'x86' ? process.env['ProgramFiles(x86)']! : process.env.ProgramFiles!;
+  let programFiles = arch === 'x86' ? process.env['ProgramFiles(x86)']! : process.env.ProgramFiles!;
+  programFiles = installMode === 'perMachine' ? programFiles : `${process.env['LOCALAPPDATA']}\\Programs`;
   const appRootFolder = path.join(programFiles, options.name!);
   const shortName = isMSICreatorOptions(options) ? options.shortName || options.name : options.name;
   const genericAumid = `com.squirrel.${shortName}.${options.exe!.replace(/\.exe$/, '')}`;
   const appUserModelId = isMSICreatorOptions(options) ? options.appUserModelId || genericAumid : genericAumid;
-  const registryRunKey = `HKLM:\\SOFTWARE\\${arch === 'x86' ? 'WOW6432Node\\' : ''}Microsoft\\Windows\\CurrentVersion\\Run`;
+  const registryRoot = installMode === 'perMachine' ? 'HKLM' : 'HKCU';
+  const registryWow = arch === 'x86' && installMode === 'perMachine' ? 'WOW6432Node\\' : '';
+  const registryRunKey = `${registryRoot}:\\SOFTWARE\\${registryWow}Microsoft\\Windows\\CurrentVersion\\Run`;
+
+  const startMenuRoot = path.join(installMode === 'perMachine' ? process.env.ProgramData! : process.env['APPDATA']!, 'Microsoft/Windows/Start Menu/Programs/');
+  const home = installMode === 'perMachine' ? process.env.Public! : process.env['home']!;
   return {
     appRootFolder,
     stubExe: path.join(appRootFolder, options.exe!),
     updateExe: path.join(appRootFolder, 'Update.exe'),
     appFolder: path.join(appRootFolder, `app-${options.version}`),
     appExe: path.join(appRootFolder, `app-${options.version}`, options.exe!),
-    startMenuShortcut: isMSICreatorOptions(options) ?  path.join(process.env.ProgramData!, `Microsoft/Windows/Start Menu/Programs/${options.shortcutFolderName || options.manufacturer}/${options.shortcutName || options.name}.lnk`) : '',
-    desktopShortcut: isMSICreatorOptions(options) ? path.join(process.env.Public!, `Desktop/${options.shortcutName || options.name}.lnk`) : '',
+    startMenuShortcut: isMSICreatorOptions(options) ?  path.join(startMenuRoot, `${options.shortcutFolderName || options.manufacturer}/${options.shortcutName || options.name}.lnk`) : '',
+    desktopShortcut: isMSICreatorOptions(options) ? path.join(home, `Desktop/${options.shortcutName || options.name}.lnk`) : '',
     appUserModelId,
     registryRunKey
   };
 };
-
